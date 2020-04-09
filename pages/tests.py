48ed0db9c61e495b44ee6c6513faaa4b0e12f8a0
Equinox-13 <quaid.johar33@gmail.com>
2020-04-09 12:53:22 +0530

    Added various tests for homepage

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        # Checks template name
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        # Checks template contents
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'I should not be there')
