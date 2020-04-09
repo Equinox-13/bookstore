037edadebaf9e994254e85e3555608b506dd24af
Equinox-13 <quaid.johar33@gmail.com>
2020-04-09 10:14:23 +0530

    Added Tests for creation of user and superuser

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase # An extension of Python’s TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will',
+            email='will@email.com',
+            password='testpass123',
+            )
+        self.assertEqual(user.username, 'will') # Checks a == b
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active) # Checks bool(x) is True
+        self.assertFalse(user.is_staff) # Checks bool(x) is False
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User =get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123',
+            )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
