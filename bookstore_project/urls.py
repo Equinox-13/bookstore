2c2d1ff7dea42e3dcf47f8588ac9b0e84f844074
Equinox-13 <quaid.johar33@gmail.com>
2020-04-10 21:12:15 +0530

    Added accounts url and added login/logout alert text

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
     path('', include('pages.urls')),
 ]
