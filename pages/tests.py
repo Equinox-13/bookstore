a88e321dc4bbe1ec06ebe344a333bccdbfd003ad
Equinox-13 <quaid.johar33@gmail.com>
2020-04-10 20:43:52 +0530

    Optimized test removed repeated lines

--- pages/tests.py
+++ pages/tests.py
@@ -1,27 +1,31 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
-
+from django.urls import reverse, resolve
+from . views import HomePageView
 
 class HomePageTests(SimpleTestCase):
 
-    def test_homepage_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get('/')
         # Checks template name
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
 
     def test_homepage_contains_correct_html(self):
-        response = self.client.get('/')
         # Checks template contents
-        self.assertContains(response, 'Homepage')
+        self.assertContains(self.response, 'Homepage')
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get('/')
-        self.assertNotContains(response, 'I should not be there')
+        # response = self.client.get('/')
+        self.assertNotContains(self.response, 'I should not be there')
+
+    def test_homepage_url_resolves_homepageview(self):
+        # ResolverMatch(func=pages.views.HomePageView, args=(), kwargs={},
+        #               url_name=home, app_names=[], namespaces=[], route=)
+        view = resolve('/')
+        self.assertEqual(view.func.__name__,HomePageView.as_view().__name__)
+
