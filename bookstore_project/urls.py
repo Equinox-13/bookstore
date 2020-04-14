52971dac550de0172f72e192fb551ad6b931b6af
Equinox-13 <quaid.johar33@gmail.com>
2020-04-14 22:57:21 +0530

    Swapped out the built-in auth app URLs for django-allauth ’s own allauth app

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -21,9 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # User management
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
 
     # Local apps
     path('', include('pages.urls')),
-    path('accounts/', include('users.urls')),
+    # path('accounts/', include('users.urls')),
 ]
