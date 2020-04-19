5e99d6c4397f783240854381e5a9c5b15371c819
Equinox-13 <quaid.johar33@gmail.com>
2020-04-19 14:04:28 +0530

    Added reviws model and modified detail view template

--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Book
+from .models import Book, Review
 
 
 class BookTests(TestCase):
 
 
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
+        )
         self.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
             price='25.00',
+        )
+        self.review = Review.objects.create(
+            book = self.book,
+            author = self.user,
+            review = 'An excellent review',
         )
 
     def test_book_listing(self):
