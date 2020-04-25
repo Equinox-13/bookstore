869eb3453d678a20d15addfc13940d7081f9dd7a
Equinox-13 <quaid.johar33@gmail.com>
2020-04-25 12:29:05 +0530

    Added custom permission for a user that can view all the detail view of the books

--- books/views.py
+++ books/views.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Book
@@ -15,8 +18,9 @@ class BookListView(LoginRequiredMixin, ListView):
     login_url = 'account_login'
 
 
-class BookDetailView(LoginRequiredMixin, DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
     login_url = 'account_login'
+    permission_required = 'books.special_status'
