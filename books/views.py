2521a8310f6a60479bbce15bfb226fd92cad8bff
Equinox-13 <quaid.johar33@gmail.com>
2020-04-26 22:30:47 +0530

    Added search results page

--- books/views.py
+++ books/views.py
@@ -24,3 +24,9 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     context_object_name = 'book'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
