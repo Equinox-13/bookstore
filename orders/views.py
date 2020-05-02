5a428ca6de631c7c41cd67834a7f5170fa58b858
Equinox-13 <quaid.johar33@gmail.com>
2020-05-02 23:07:03 +0530

    Added stripe order processed page

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.conf import settings
+import stripe
+
+stripe.api_key = settings.STRIPE_TEST_PUBLISHABLE_KEY
 
 
 class OrdersPageView(TemplateView):
     template_name = 'orders/purchase.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stripe_key'] = settings.STRIPE_TEST_PUBLISHABLE_KEY
+        return context
+
+def charge(request):
+    if request.method == 'POST':
+        charge = stripe.Charge.create(
+            amount=3900,
+            currency='usd',
+            description='Purchase all books',
+            source=request.POST['stripeToken']
+        )
+        return render(request, 'orders/charge.html')
