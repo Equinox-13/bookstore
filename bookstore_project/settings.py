c1b48ca1ec78b731882576855fbfb135b524ebb3
Equinox-13 <quaid.johar33@gmail.com>
2020-05-04 23:00:18 +0530

    New updates for heroku deployment

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%zp%mjl3&(etggkc-#%pinqd0@7qaw+7r&ux$%e1d*5v@a$@^z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
