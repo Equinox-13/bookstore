2f73e2e67e9c07cda35047b7f29fb9372cf39112
Equinox-13 <quaid.johar33@gmail.com>
2020-05-06 22:29:58 +0530

    switched to debug mode

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%zp%mjl3&(etggkc-#%pinqd0@7qaw+7r&ux$%e1d*5v@a$@^z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
